9741e89e0bb999a838a96c41685290628c84793b
dams <dams@zarb.org>
2014-01-24 20:27:56 +0100

    updated doc

--- lib/Moo/Lax.pm
+++ lib/Moo/Lax.pm
@@ -2,6 +2,13 @@ package Moo::Lax;
 
 #ABSTRACT: By default Moo turns all warnings to fatal warnings. C<Moo::Lax> is exactly the same as C<Moo>, except that it doesn't turn all warnings to fatal warnings in the calling module.
 
+=head1 SYNOPSIS
+
+  # instead of use Moo;
+  use Moo::Lax;
+
+=cut
+
 require strictures;
 my $orig = \&strictures::import;
 require Moo;
--- lib/Moo/Role/Lax.pm
+++ lib/Moo/Role/Lax.pm
@@ -2,6 +2,13 @@ package Moo::Role::Lax;
 
 #ABSTRACT: By default Moo::Role turns all warnings to fatal warnings. This module is exactly the same as Moo::Role, except that it doesn't turn all warnings to fatal warnings in the calling module.
 
+=head1 SYNOPSIS
+
+  # instead of use Moo::Role;
+  use Moo::Role::Lax;
+
+=cut
+
 require strictures;
 my $orig = \&strictures::import;
 require Moo;
