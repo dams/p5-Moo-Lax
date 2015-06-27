8c01dcfc0db64b9bf297306843ea5b733dda87c0
Damien Krotkine <dams@zarb.org>
2015-06-27 20:25:47 +0200

    add deprecated notice

--- lib/Moo/Role/Lax.pm
+++ lib/Moo/Role/Lax.pm
@@ -2,6 +2,15 @@ package Moo::Role::Lax;
 
 #ABSTRACT: Loads Moo::Role without turning warnings to fatal.
 
+=head1 DEPRECATED
+
+With the release of L<Moo> version 2, C<use Moo> no longer imports
+L<strictures> by default and therefore warnings are not fatalised unless
+an explicit C<use strictures> is added to the code.
+
+As such, this module is no longer required - simply update your dependency
+on Moo to version 2 and switch back to plain C<use Moo> in your classes.
+
 =head1 DESCRIPTION
 
 By default Moo::Role turns all warnings to fatal warnings. This module is
